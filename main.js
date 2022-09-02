de2f68fb659b1d4a5334b319ad43c4dbfa15fa2c
Luke Jennings <lukejennings123@gmail.com>
2022-09-02 10:39:19 +0100

    added search functionality for locations

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-const test = document.getElementById('infoTest');
+const pushToScreen = document.getElementById('infoTest');
+const inputBox = document.getElementById('inputBox');
+const submitLocationButton = document.getElementById('submitLocationButton');
 
 
+submitLocationButton
 const fetchAPI = async function weatherAPIFetch(){
-const apiFetch = await fetch('https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6a1ec683efc2b775407037c6b2204412', {mode:'cors'});
-const jsonConversion = await apiFetch.json();
-return console.log(jsonConversion), test = jsonConversion;
+const apiFetch = await fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputBox.value+'&APPID=6a1ec683efc2b775407037c6b2204412', {mode:'cors'})
+const jsonConversion = await apiFetch.json()
+return jsonConversion, pushToScreen.textContent = jsonConversion.name, console.log(jsonConversion.name)
+
 }
-fetchAPI().catch(err => {
-    console.log('ERROR')
+
+// fetchAPI().catch(err => {
+//     console.error(err)
+// });
+
+submitLocationButton.addEventListener('click', () => {
+    fetchAPI()
+    console.log(inputBox.value)
 });
 
+
+
+//need to get the data from a specific city. Also enable a text box and button to search for a user input city
