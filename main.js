f1de681b3928a56cbadd7838959ac8d69a0a44d6
Luke Jennings <lukejennings123@gmail.com>
2022-09-02 16:36:08 +0100

    added API pull function

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ submitLocationButton
 const fetchAPI = async function weatherAPIFetch(){
 const apiFetch = await fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputBox.value+'&APPID=6a1ec683efc2b775407037c6b2204412', {mode:'cors'})
 const jsonConversion = await apiFetch.json()
-return jsonConversion, pushToScreen.textContent = jsonConversion.name, console.log(jsonConversion.name)
+return jsonConversion, console.log(jsonConversion.name),console.log(jsonConversion), console.log(jsonConversion.main.feels_like,jsonConversion.main.humidity);
+// ,pushToScreen.textContent = jsonConversion.name
 
 }
 
@@ -16,10 +17,19 @@ return jsonConversion, pushToScreen.textContent = jsonConversion.name, console.l
 // });
 
 submitLocationButton.addEventListener('click', () => {
-    fetchAPI()
+    fetchAPI().catch(function (){
+        console.log('error)');
+    });
     console.log(inputBox.value)
 });
 
 
 
-//need to get the data from a specific city. Also enable a text box and button to search for a user input city
+//need to get the data from a specific city. Also enable a text box and button to search for a user input city - done
+// jsonConversion.main.feels_like is given in kelvin so minus -273.15 from it to get it in degree
+// jsonConversion.main.temp is the actual temp in kelvin  
+// jsonConversion.main.humidity is the actual humidity in percent  
+// jsonConversion.wind.speed is the actual wind speed in km/h 
+// jsonConversion.list.pop is the actual wind speed in km/h 
+
+// jsonConversion.clouds.all is the cloud amount in percentage. The percentage range can be done on the site https://openweathermap.org/weather-conditions 
